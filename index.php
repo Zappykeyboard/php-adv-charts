9803c23364b34d060c7ef1c878ecfd27bdbac01c
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-09-10 16:15:27 +0200

    aggiunto html

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- FONT: LATO -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+
+    <!--CHART JS-->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0/dist/Chart.min.js"></script>
+    <!-- JS: JQUERY -->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- JS: MOMENT -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment-with-locales.min.js"></script>
+
+    <!-- JS: HANDLEBARS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.0/handlebars.min.js" charset="utf-8"></script>
+    <!-- TEMPLATE: MESSAGE MENU -->
+    <script id="item-template" type="text/x-handlebars-template">
+    </script>
+
+    <!-- CSS: MY STYLE -->
+    <link rel="stylesheet" href="style.css">
+    <!-- JS: MY SCRIPT -->
+    <script src="script.js" charset="utf-8"></script>
+
+    <title>php-adv-charts</title>
+  </head>
+  <body>
+    <h1>Hello world</h1>
+  </body>
+</html>
